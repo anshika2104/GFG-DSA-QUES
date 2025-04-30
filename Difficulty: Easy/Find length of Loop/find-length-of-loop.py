50bfcbf8ae7f29e3c6b911e1d0181ba468d04f9f
anshika2104 <91836420+anshika2104@users.noreply.github.com>
2025-04-30 13:37:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find length of Loop/find-length-of-loop.py
@@ -0,0 +1,86 @@
+#User function Template for python3
+
+'''
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    # Function to find the length of a loop in the linked list.
+    def CountNode(self,node):
+        c=1
+        cur=node
+        while cur.next!=node:
+            c+=1
+            cur=cur.next
+        return c
+
+    def countNodesInLoop(self, head):
+        #code here
+        slow,fast=head,head
+        while slow and fast and fast.next is not None:
+            slow=slow.next
+            fast=fast.next.next
+            if slow == fast:
+                return self.CountNode(slow)
+        return 0
+        
+
+
+#{ 
+ # Driver Code Starts
+import sys
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def print_list(node):
+    while node:
+        print(node.data, end=' ')
+        node = node.next
+    print()
+
+
+def loop_here(head, pos):
+    if pos == 0:
+        return
+
+    walk = head
+    for _ in range(1, pos):
+        walk = walk.next
+
+    tail = head
+    while tail.next:
+        tail = tail.next
+
+    tail.next = walk
+
+
+if __name__ == "__main__":
+    input = sys.stdin.read
+    data = input().split('\n')
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        arr = list(map(int, data[index].split()))
+        k = int(data[index + 1])
+        head = Node(arr[0])
+        tail = head
+        for value in arr[1:]:
+            tail.next = Node(value)
+            tail = tail.next
+        loop_here(head, k)
+        ob = Solution()
+        res = ob.countNodesInLoop(head)
+        print(res)
+        print("~")
+        index += 2
+
+# } Driver Code Ends
