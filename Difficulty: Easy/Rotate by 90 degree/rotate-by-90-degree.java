81340d8b7b17cd052af953ebc9a3b57d2560656b
anshika2104 <91836420+anshika2104@users.noreply.github.com>
2025-06-05 18:40:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate by 90 degree/rotate-by-90-degree.java
@@ -0,0 +1,31 @@
+// User function Template for Java
+
+class Solution {
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int mat[][]) {
+        // code here
+        int n=mat.length;
+        for(int i=0;i<n;i++){
+            reverser(mat[i]);
+        }
+              for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+    }
+    static void reverser(int[] row) {
+        int left = 0, right = row.length - 1;
+        while (left < right) {
+            int temp = row[left];
+            row[left] = row[right];
+            row[right] = temp;
+            left++;
+            right--;
+        }
+   
+        
+    }
+}
